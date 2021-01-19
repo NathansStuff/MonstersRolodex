0e4a269d6de7ddb5abfe29c77afe371349251791
Nathanstuff <n.odonnell@hotmail.com>
2021-01-20 06:21:02 +1100

    card list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -10,18 +11,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(
-      "https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users => this.setState({ monsters: users }));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }))
+      .catch((error) => console.log("Error in calling data"));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <CardList name="">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
