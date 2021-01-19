e5d6f83af9053ad7a8fd5c82f84dabf886a915b3
Nathanstuff <n.odonnell@hotmail.com>
2021-01-20 07:42:21 +1100

    search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import "./App.css";
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
+/** @format */
+
+import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }))
-      .catch((error) => console.log("Error in calling data"));
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(error => console.log('Error in calling data'));
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
