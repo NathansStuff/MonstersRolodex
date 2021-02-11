8a07aae84088a30097e6af9111a4f9d4703d66ba
Nathanstuff <n.odonnell@hotmail.com>
2021-02-12 00:42:58 +1100

    errors

--- src/App.js
+++ src/App.js
@@ -43,3 +43,50 @@ class App extends Component {
 }
 
 export default App;
+
+
+import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(error => console.log('Error in calling data'));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    // shorthand for (const monsters = this.state.monsters);
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+    return (
+      <div className='App'>
+      <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
